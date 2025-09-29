c7cd1910f1d224754b8867cda58185fdae158f36
aminechairi <aminemesseiyah@gmail.com>
2025-09-29 09:16:14 +0100

    Login page.

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Dashboard() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-3xl">Home page</h1>
+      <h1 className="text-3xl">Dashboard page</h1>
     </div>
   );
 }
